f2c83a370eb4be1f80264d1cd5a83de5be2b5858
Dorian <dkevelop@gmail.com>
2022-07-19 16:08:24 +0300

    uppdate

--- js/components/swiper.js
+++ js/components/swiper.js
@@ -2,7 +2,11 @@ const swiper = new Swiper(document.querySelector('.swiper'), {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-
+    // slidesPerView: 'auto',
+    parallax: true,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    // rewind: true,
     // If we need pagination
     // pagination: {
     //     el: '.swiper-pagination',
@@ -10,14 +14,46 @@ const swiper = new Swiper(document.querySelector('.swiper'), {
     autoplay: {
         delay: 5000,
     },
+    mousewheel: {
+        invert: true,
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
     },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+breakpoints: {
+    320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        direction:'vertical',
+        allowTouchMove: true,
+    },
+    480: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        direction:'vertical',
+        allowTouchMove: true,
+    },
+    640: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+        allowTouchMove: true,
+        // direction:'vertical'
+
+    }
+    },
+
+
 });
+
